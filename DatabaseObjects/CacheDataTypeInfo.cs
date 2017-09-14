c469de1eb233daaa9dd56fb3ab10b9b76aacc740
internet@swifmobi.com <internet@swifmobi.com>
2017-09-14 12:10:08 +0200

    Add project files.

---
+++ DatabaseObjects/CacheDataTypeInfo.cs
@@ -0,0 +1,20 @@
+﻿
+using OutSystems.HubEdition.Extensibility.Data;
+using OutSystems.HubEdition.Extensibility.Data.DatabaseObjects;
+
+namespace OutSystems.HubEdition.DatabaseProvider.Cache.Introspection {
+    public class CacheDataTypeInfo : IDataTypeInfo {
+
+        public CacheDataTypeInfo(DBDataType type, string sqlDataType, int length, int decimals) {
+            Type = type;
+            SqlDataType = sqlDataType;
+            Length = length;
+            Decimals = decimals;
+        }
+
+        public DBDataType Type { get; set; }
+        public string SqlDataType { get; private set; }
+        public int Length { get; set; }
+        public int Decimals { get; private set; }
+    }
+}
